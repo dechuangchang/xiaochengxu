694b8dffc967a634f19322c16265b31580cf2106
常德闯 <changdechuang@gmail.com>
2018-05-15 13:35:36 +0800

    每日散文

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,68 @@
 //index.js
 //获取应用实例
-const app = getApp()
-
+const app = getApp();
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  data:{
+    text:null
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  onLoad(){
+    console.log('生命周期函数--监听页面加载')
+    this.getText();
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+  onReady(){
+    console.log('生命周期函数--监听页面初次渲染完成')
+    console.log(this.data)
+  },
+  onShow(){
+    console.log('生命周期函数--监听页面显示')
+  },
+  onHide(){
+    console.log('生命周期函数--监听页面隐藏')
+  },
+  onUnload(){
+    console.log('生命周期函数--监听页面卸载')
+  },
+  onPullDownRefresh(){
+    console.log('页面相关事件处理函数--监听用户下拉动作')
+    this.getText()
+  },
+  onReachBottom(){
+    console.log('页面上拉触底事件的处理函数')
+  },
+  onPageScroll(ev){
+    // console.log(ev)
+
+  },
+  getText(){
+    let that = this;
+    wx.request({
+      url: 'https://interface.meiriyiwen.com/article/random',
+      data: {
+        dev:1
+      },
+      header: {
+        'content-type': 'application/json' 
+      },
+      success: function (res) {
+        let content = res.data.data.content;
+        res.data.data.content = content.replace(/<p>/g, '').split('</p>')
+        that.setData({
+          text: res.data.data
+        },()=>{
+          wx.pageScrollTo({
+            scrollTop: 0,
+            duration: 300
+          })
+          wx.stopPullDownRefresh()
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
+  },
+  onShareAppMessage: function () {
+    console.log('这是分享配置')
+    return {
+      title: '常德闯的每日阅读',
+      path: 'pages/index/index'
+    }
   }
 })
